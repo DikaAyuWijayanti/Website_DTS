16d4916f9a78932efacac363eae02f461bea9e5c
DikaAyuWijayanti <dikaayuwijayanti1234@gmail.com>
2021-11-05 03:33:58 +0700

    editkoneksi

--- inc/conn.php
+++ inc/conn.php
@@ -1,7 +1,7 @@
 <?php
-    define('HOST', 'localhost');
-    define('USERNAME', 'root');
-    define('PASSWORD', '');
+    define('HOST', '10.0.0.246');
+    define('USERNAME', 'admin');
+    define('PASSWORD', 'Rdk95@al1');
     define('DB_NAME', 'website_dika');
 
     $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DB_NAME);
